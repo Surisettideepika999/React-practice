b1377177c5d254bf93a01992c7f98b4c501020cb
Surisettideepika999 <surisettideepika999@gmail.com>
2025-07-30 17:31:50 +0530

    Revert "project setup"
    This reverts commit f849bf716756a43717553a1de2644e97ee8e5e94.

--- mind-space/src/utils/quotes.js
+++
@@ -1,7 +0,0 @@
-// utils/quotes.js
-export const quotes = [
-  "You are enough just as you are.",
-  "Breathe in calm, breathe out stress.",
-  "Progress, not perfection.",
-  "Small steps every day lead to big changes."
-];
